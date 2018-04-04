dbd58f477b950c3a447044daba29490b8ec20248
innovationchef <ankit.lohani.iitkgp@gmail.com>
2018-04-05 01:34:33 +0530

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # concept-map-generator
+
+1. Clone the repo to local machine.
+
+2. Download GLoVe vectors from Stanford's website - 
+```
+wget https://nlp.stanford.edu/data/wordvecs/glove.6B.zip
+```
+
+3. Unzip the file and keep it in the main directory of our codebase with folder name - "glove.6B". It will contain 4 text files - glove.6B.50d.txt, glove.6B.100d.txt, glove.6B.200d.txt, glove.6B.300d.txt
+
+4. Set up Solr - https://www.howtoforge.com/tutorial/how-to-install-and-configure-solr-on-ubuntu-1604/ 
+
+5. Create a Solr Core 
+```
+sudo su - solr -c "/opt/solr/bin/solr create -c glove"
+```
+
+6. Setup the core
+```
+cd setup
+./setup-solr.py
+cd ..
+```
+
+7. Install the dependencies - 
+```
+pip install -r requirements.txt
+```
+
+8. Index the vectors in the core - the indexer script is also in the setup folder
+```
+./solr-indexer.py
+cd ..
+```
+
+TIP: If you wish to delete the documents from a core, you may do the same by running the following link on your web browser - 
+```
+localhost:8983/solr/glove/update?commit=true&stream.body=%3Cdelete%3E%3Cquery%3E*:*%3C/query%3E%3C/delete%3E
+```
+
+9. Run the main script - 
+```
+python graphbuilder.py
+``` 
--- graphbuilder.py
+++ graphbuilder.py
@@ -9,8 +9,10 @@ raw_text = "Planets move on path called orbits. Mercury is nearest to the sun. M
 # # raw_text = "Plants are an important source of food for both humans and animals. Grains like rice and wheat, fruits and vegetables, pulses, sugar and spices comes from a variety of plants. They also provide us oils like sunflower, mustard and groundnut oil which is used as a cooking fuel. Half of the food that we eat daily comes from just two crops: rice and wheat. Rice is used to make boiled rice, idlis and dosa. Wheat is grinded to make chapattis, noodles, bread etc. Not just solid items but plants are also source of liquid items like tea and coffee."
 # raw_text = "Pluto is the smallest planet in the universe. Pluto is the last planet in solar system."
 # raw_text = "The biggest planet is Jupiter. Jupiter is made of gas and is so big that you could fit 1,321 planets the size of Earth inside it. There is even a storm on Jupiter that is bigger than Earth - this storm has been blowing for hundreds of years and is called the Great Red Spot."
+
 relations = extractrelations.extractRel(raw_text, endpoint)
 print relations
+
 # relations = [('mercury', ' complete', ' one round along its orbit')]
 
 sub, pred, rel = list(), list(), list()
